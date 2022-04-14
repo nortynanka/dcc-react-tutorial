b728d2852f423835f6bb35fd10bf153fa4495896
Kayl Guise <kaylee.guise@gmail.com>
2022-04-14 12:02:56 -0500

    Parts 1 and 2 of tutorial completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <table>
+       <thead>
+         <tr>
+           <th>Entry Number</th>
+           <th>Weight</th>
+           <th>Date</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td>1</td>
+           <td>175</td>
+           <td>11-23-2021</td>
+         </tr>
+       </tbody>
+     </table>
     </div>
   );
 }
