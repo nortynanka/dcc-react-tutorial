68a1bb85ca414f4ac6edfc50c1efc894ccf34ccf
Kayl Guise <kaylee.guise@gmail.com>
2022-04-15 11:13:01 -0500

    Completed part 7 of the tutorial.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 import AddEntryForm from "./Components/AddEntry/AddEntryForm";
+import EntriesChartTracker from "./Components/EntriesChartTracker/EntriesChartTracker";
 
 
 function App() {
@@ -16,6 +17,7 @@ function addNewEntry(entry) {
     <div>
       <DisplayEntries parentEntries={entries} />
       <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
---
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { Chart } from "react-google-charts";
+
+const EntriesChartTracker = (props) => {
+
+    const [chartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        let tempChartData = props.parentEntries.map(entry => {
+            return [entry.date, entry.weight];
+        });
+        setChartData(tempChartData);
+    }, [props.parentEntries]);
+
+    return ( <Chart
+        chartType="LineChart"
+        data={[["Date", "Weight"], ...chartData]}
+        width="100%"
+        height="400px"
+        legendToggle
+      /> 
+    );
+}
+ 
+export default EntriesChartTracker;
